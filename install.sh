33a22f0156d11ec63e993db5f27198d57548056f
Hùng Ngọc Phát <49612476+hungngocphat01@users.noreply.github.com>
2021-09-25 13:12:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
+set +x
 
 echo WARNING: YAY must be installed prior to executing this file!
 
 # Install all pacman packages
+echo ============= INSTALL PACMAN PACKAGES =============
 pacman_pkgs=(
 	zsh
 	base-devel
@@ -14,25 +16,16 @@ pacman_pkgs=(
 sudo pacman --noconfirm -Sy $(echo $pacman_pkgs)
 
 # Change shell 
-chsh -s $(which zsh) hiraki
+chsh -s $(which zsh) $LOGNAME
 
+echo ============= CONFIGURE GIT =============
 # Configure git 
 git config --global user.name hungngocphat01
 git config --global user.email hungngocphat01@gmail.com
 git config credential.helper store
 
-# Install oh-my-zsh
-sed -i "s/ZSH_THEME=.*/ZSH_THEME='af-magic'/g"
-
-# Configure aliases
-cat >> ~/.zshrc <<EOF
-export TERM=xterm-256color
-alias rm='rm -i'
-alias open='xdg-open'
-export LANG=en_US.UTF-8
-EOF
-
 # Configure vim
+echo ============= CONFIGURE ZSH ALIAS =============
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cat > ~/.vimrc <<EOF
@@ -73,4 +66,15 @@ sudo systemctl enable --now vmware.service vmware-networks.service
 
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i "s/ZSH_THEME=.*/ZSH_THEME='af-magic'/g"
 
+echo ============= CONFIGURE ZSH ALIAS =============
+# Configure aliases
+cat >> ~/.zshrc <<EOF
+export TERM=xterm-256color
+alias rm='rm -i'
+alias open='xdg-open'
+export LANG=en_US.UTF-8
+EOF
+
+set -x
